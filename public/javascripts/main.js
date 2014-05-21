e8546e03aea0899bfe38eecb548b3c7334c386bd
andrewhassan <andrewihassan@gmail.com>
2014-05-21 10:02:33 -0400

    Added 2 projects

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
   $(".horizontal-line").addClass("show");
+
+  // Clear the modal cache on close
+  $('body').on('hidden.bs.modal', function () {
+    $('#project_modal').removeData('bs.modal');
+  });
 });
